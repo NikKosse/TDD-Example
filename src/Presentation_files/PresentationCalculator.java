172d01f0781316bb2dfbf860cff3e7bbe1320c77
nikolas <nikolas.kosse@gmail.com>
2015-11-19 00:16:07 -0600

    Added empty files for the presentation- calculator and calculator test

---
+++ src/Presentation_files/PresentationCalculator.java
@@ -0,0 +1,33 @@
+package Presentation_files;
+
+import java.util.ArrayList;
+
+/**
+ * Stubbed methods for calculations
+ */
+public class PresentationCalculator {
+
+    public double averageNumbers(ArrayList<Integer> numbers){
+        double answer = 0;
+
+        return answer;
+    }
+
+    public int addNumbers(ArrayList<Integer> numbers){
+        int answer = 0;
+
+        return answer;
+    }
+
+    public int subtractNumbers(ArrayList<Integer> numbers){
+        int answer = numbers.get(0);
+
+        return answer;
+    }
+
+    public int multiplyNumbers(ArrayList<Integer> numbers){
+        int answer = numbers.get(0);
+
+        return answer;
+    }
+}
---
+++ src/Presentation_files/PresentationCalculatorTest.java
@@ -0,0 +1,25 @@
+package Presentation_files;
+
+import junit.framework.TestCase;
+
+/**
+ * Empty test class for presentation
+ */
+public class PresentationCalculatorTest extends TestCase {
+
+    public void testAverageNumbers() throws Exception {
+
+    }
+
+    public void testAddNumbers() throws Exception {
+
+    }
+
+    public void testSubtractNumbers() throws Exception {
+
+    }
+
+    public void testMultiplyNumbers() throws Exception {
+
+    }
+}
